d529ef432bae4d308a11416fc10e5deb42bbfeb1
Marco Casula <mc@clickbit.net>
2016-09-20 15:25:13 -0500

    add build
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- test/main.js
+++ test/main.js
@@ -1,3 +1,10 @@
-requirejs(["../build/index.js"], function(lib) {
-  console.log(lib)
+requirejs(["../build/index.js"], function(historyUtils) {
+  console.log(historyUtils)
+  // historyUtils.parseXml(xmlSchema, (rawSchema) => {
+  //   historyUtils.parseXml(xmlHistory, (rawHistory) => {
+  //     const schema = historyUtils.parseSchema(rawSchema)
+  //     const history = historyUtils.parseHistory(rawHistory)
+  //     console.dir(historyUtils.makeHistory(schema, history))
+  //   })
+  // })
 });
