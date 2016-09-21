9fb87044cf122115fda96d9a24a2e6208ae63de6
Marco Casula <mc@clickbit.net>
2016-09-21 10:00:14 -0500

    consider winnings add sport test
    Signed-off-by: Marco Casula <mc@clickbit.net>

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ const parseSchema = lib.parseSchema
 const makeHistory = lib.makeHistory
 
 const xmlSchema = fs.readFileSync('./schema.xml').toString()
-const xmlHistory = fs.readFileSync('./critters.xml').toString()
+const xmlHistory = fs.readFileSync('./critters-sport.xml').toString()
 
 const parseXml = (xml, cb) => {
   parser.parseString(xml, (err, result) => {
